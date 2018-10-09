590afdf96828cd97ac4d3646f3f8c4de83593c6e
ejwall <emily.wall@maine.edu>
2018-10-09 12:50:16 -0400

    ex3 complete

--- project-3/ex3/lrCostFunction.m
+++ project-3/ex3/lrCostFunction.m
@@ -36,8 +36,14 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+% y' * h = sum(y .* h)
 
+J = ...
+    (1/m)*(-y' * log(sigmoid(X * theta)) - (1 - y') * log(1-sigmoid(X * theta)))... 
+    + (lambda/(2*m))*sum(theta(2:end).^2);
 
+grad = (1/m)*X'*(sigmoid(X*theta)-y) + (lambda/m) * theta;
+grad(1) = (1/m)*X(:,1)'*(sigmoid(X*theta)-y);
 
 
 
